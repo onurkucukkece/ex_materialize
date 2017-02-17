86ddcf8a278179ff2208c436e2d307c787fd5739
mistim <m.mistim@gmail.com>
2017-02-17 23:28:04 +0200

    - init commit; - add task install;

---
+++ lib/materialize.ex
@@ -0,0 +1,18 @@
+defmodule Materialize do
+  @moduledoc """
+  Documentation for Materialize.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Materialize.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/mix/tasks/install.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Materialize.Install do
+	use Mix.Task
+
+	@moduledoc """
+	"""
+
+	@shortdoc ""
+	def run(_) do
+		IO.puts "Test install"
+	end
+end
