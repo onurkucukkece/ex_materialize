ece3f783e5857a5e06755eba8fd2b9baa7b96cab
mistim <m.mistim@gmail.com>
2017-02-17 23:37:42 +0200

    - task install;

--- lib/mix/tasks/install.ex
+++ lib/mix/tasks/install.ex
@@ -2,10 +2,31 @@ defmodule Mix.Tasks.Materialize.Install do
 	use Mix.Task
 
 	@moduledoc """
+	Install material-css
 	"""
 
-	@shortdoc ""
+	@doc "start task"
 	def run(_) do
 		IO.puts "Test install"
+	end
+
+	@doc ""
+	defp assets do
+		
+	end
+
+	@doc "upload resource"
+	defp upload_source do
+		
+	end
+
+	@doc "move dist to web/static/vendor/materialize"
+	defp move_dist do
+		
+	end
+
+	@doc "configure brunch"
+	defp brunch_config do
+		
 	end
 end
